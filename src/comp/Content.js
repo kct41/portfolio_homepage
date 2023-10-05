3d1dd407b24970c5849ee9406f540900156bd5d0
SOMI <som2mos@naver.com>
2023-10-06 03:35:25 +0900

    1006 - main visual

--- src/comp/Content.js
+++ src/comp/Content.js
@@ -1,19 +1,37 @@
 import img from '../img/KakaoTalk_Photo_2023-10-04-14-29-41.png';
-import {SectionsContainer, Section} from 'react-fullpage';
-function Content(){
+import {ScrollToTopOnMount ,SectionsContainer, Section} from 'react-fullpage';
+import Typed from 'typed.js';
+import React from 'react';
+
+function Content() {
+
+    const el = React.useRef(null);
+
+    React.useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ['성실 빼면 시체', '책임감 있는', '웹퍼블리셔',],
+            typeSpeed: 70,
+            backSpeed: 30,
+        });
+
+        return () => {
+            // Destroy Typed instance during cleanup to stop animation
+            typed.destroy();
+        };
+    }, []);
 
     let options = {
-        sectionClassName:     'section',
-        anchors:              ['sectionOne', 'sectionTwo', 'sectionThree'],
-        scrollBar:            false,
-        navigation:           true,
-        verticalAlign:        false,
-        sectionPaddingTop:    '50px',
-        sectionPaddingBottom: '50px',
-        arrowNavigation:      true
+        sectionClassName: 'section',
+        anchors: ['sectionOne', 'sectionTwo', 'sectionThree'],
+        scrollBar: false,
+        navigation: true,
+        verticalAlign: false,
+        sectionPaddingTop: '80px',
+        sectionPaddingBottom: '80px',
+        arrowNavigation: true,
     };
 
-    return(
+    return (
         // <div className="Content">
         //     <section className="home">
         //         <div>
@@ -33,13 +51,16 @@ function Content(){
             <Section>
                 <section className="home">
                     <div>
-                        <div className="img_circle">
-                            <img alt="img" src={img}/>
+                        <div>
+                            <div className="img_circle">
+                                <img alt="img" src={img}/>
+                            </div>
                         </div>
-                        <div className="text_box">반가워요!<br/>
+                        <div className="text_box">안반가워요!<br/>
                             저는<br/>
-                            <span>웹퍼블리셔</span><br/>
-                            김소미입니다.</div>
+                            <span ref={el}/><br/>
+                            김소미입니다.
+                        </div>
                     </div>
                 </section>
             </Section>
--- src/comp/Nav.js
+++ src/comp/Nav.js
@@ -6,16 +6,17 @@ function Nav() {
     const app = document.querySelector('.App');
     function night(){
         app.classList.toggle('night');
+        console.log("why");
     }
 
     return (
         <nav>
             <Link to="/"><h1>Logo</h1></Link>
             <ul>
-                <Link to="ss"><li>HOME</li></Link>
-                <Link to="ss"><li>ABOUT</li></Link>
-                <Link to="ss"><li>PORTFOLIO</li></Link>
-                <Link to="ss"><li>GIT</li></Link>
+                <Link to="/"><li>HOME</li></Link>
+                <Link to="/about"><li>ABOUT</li></Link>
+                <Link to="/portfolio"><li>PORTFOLIO</li></Link>
+                <Link to="/git"><li>GIT</li></Link>
                 <li className="dayNight" onClick={night}>
                     <FontAwesomeIcon className="icon" icon={faMoon}/>
                     <FontAwesomeIcon className="icon" icon={faSun}/>
